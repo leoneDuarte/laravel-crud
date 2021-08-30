093650e08edca7cfec18afe919230ff09edb5849
Leone <falarcomleone@gmail.com>
2021-08-30 00:54:52 -0300

    S01 novas alterações

--- resources/views/admin/cursos/editar.blade.php
+++ resources/views/admin/cursos/editar.blade.php
@@ -1,14 +1,10 @@
-
 @extends('layout.site')
-
 @include('layout._includes.topo')
-
 @section('titulo','adicionar curso')
-    
+   
 @section('corpo')
 <div class="container">
     <h3 class="center">EDITAR CURSO</h3>
-
     <div class="row">
         <form method="POST" action="{{route('admin.cursos.atualizar',$registro->curso_id)}}">            
             {{csrf_field() }}
@@ -16,9 +12,7 @@
             @include('layout._includes.form')
         </form>
     </div>
-
 </div>
 @endsection
-
 
 @include('layout._includes.footer')
--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,9 @@
 Route::get('/', function () {
     return view('home');
 });
-
 Route::get('/admin/cursos',['as'=>'admin.cursos', 'uses'=>'Admin\CursoController@index']);
 Route::get('/admin/cursos/adicionar',['as'=>'admin.cursos.adicionar', 'uses'=>'Admin\CursoController@adicionar']);
 Route::post('/admin/cursos/salvar',['as'=>'admin.cursos.salvar', 'uses'=>'Admin\CursoController@salvar']);
 Route::get('/admin/cursos/editar/{curso_id}',['as'=>'admin.cursos.editar', 'uses'=>'Admin\CursoController@editar']);
-
 Route::put('/admin/cursos/atualizar/{curso_id}',['as'=>'admin.cursos.atualizar', 'uses'=>'Admin\CursoController@atualizar']);
-
 Route::get('/admin/cursos/deletar/{curso_id}',['as'=>'admin.cursos.deletar', 'uses'=>'Admin\CursoController@deletar']);
